da58ceacf1b86369ddbd9ee224c93c41fde1e58a
Arda Yazıcı <arda18914@gmail.com>
2026-01-06 15:19:29 +0300

    Update TaskViewModel.kt

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -7,6 +7,9 @@ class TaskViewModel : ViewModel() {
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
 
+    val task1 : Task? by rememberSaveable {mutableStateOf(null)}
+    val task2 : Task? by rememberSaveable {mutableStateOf(null)}
+
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
     }
